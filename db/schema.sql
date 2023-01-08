8e92f1576e2be6dd00a0b3747204e4aa026c410f
Elaine Bautista <elainebautista@Elaines-MacBook-Pro.local>
2023-01-08 11:50:26 -0500

    Added role table and data

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,20 @@
+-- Create DB
 DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 
 USE employee_db;
 
+-- Create Department Table
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   department_name VARCHAR(30) NOT NULL
+);
+
+-- Create Role Table
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL, 
+    salary DECIMAL NOT NULL,
+    department_id INT, 
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -5,5 +5,10 @@ VALUES ("Sales"),
        ("Finance"),
        ("Operations");
 
-
+INSERT INTO role (title, department_name, salary)
+VALUES ("Sales Manager", "Sales", 80000),
+       ("Marketing Manager", "Marketing", 90000),
+       ("Software Engineer", "IT", 150000),
+       ("Finance Manager", "Finance", 120000),
+       ("Operations Manager", "Operations", 75000);
        
