847c8e3098f271ad6158a97a6923999e6f8bcc0e
Elaine Bautista <elainebautista@Elaines-MacBook-Pro.local>
2023-01-08 12:07:22 -0500

    Added employee table and data

--- db/schema.sql
+++ db/schema.sql
@@ -18,3 +18,15 @@ CREATE TABLE role (
     department_id INT, 
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
+
+-- Create Employee Table
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    salary_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,4 @@
+-- Department
 INSERT INTO department (department_name)
 VALUES ("Sales"),
        ("Marketing"),
@@ -5,10 +6,20 @@ VALUES ("Sales"),
        ("Finance"),
        ("Operations");
 
+-- Role
 INSERT INTO role (title, department_name, salary)
 VALUES ("Sales Manager", "Sales", 80000),
        ("Marketing Manager", "Marketing", 90000),
+       ("Engineering Manager", "IT", 250000),
        ("Software Engineer", "IT", 150000),
        ("Finance Manager", "Finance", 120000),
        ("Operations Manager", "Operations", 75000);
        
+-- Employee
+INSERT INTO employee (first_name, last_name, role_id, department_name, salary, manager_id)
+VALUES ("Mike", "Chen", 1, 1, 1, null),
+       ("James", "Moore", 2, 2, 2, null),
+       ("Michael", "Donald", 3, 3, 3, null),
+       ("Gary", "Lee", 4, 4, 4, 3),
+       ("Samantha", "Price", 5, 5, 5, null),
+       ("Catherine", "Sutton", 6, 6, 6, null),
